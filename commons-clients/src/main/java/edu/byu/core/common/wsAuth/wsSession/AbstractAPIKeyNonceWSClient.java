d548ca5d67808c2744c247f275dd01b2b959aa47
tefreestone <tefreestone@gmail.com>
2014-06-12 11:00:37 -0600

    refactoring abstract classes

---
+++ commons-clients/src/main/java/edu/byu/core/common/wsAuth/wsSession/AbstractAPIKeyNonceWSClient.java
@@ -0,0 +1,104 @@
+package edu.byu.core.common.wsAuth.wsSession;
+
+
+import edu.byu.core.common.wsAuth.AbstractWSClient;
+import edu.byu.core.common.wsAuth.api.CredentialClient;
+import edu.byu.core.common.wsAuth.api.NonceClient;
+import edu.byu.core.common.wsAuth.model.hibernate.WsNonce;
+import edu.byu.core.common.wsAuth.model.hibernate.WsSessionCredential;
+import edu.byu.core.common.wsAuth.model.security.SessionNonceHmacCredential;
+import org.slf4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+
+
+public abstract class AbstractAPIKeyNonceWSClient extends AbstractWSClient {
+
+    private final Logger LOG = org.slf4j.LoggerFactory.getLogger(this.getClass());
+
+    private CredentialClient credentialClient;
+    private NonceClient nonceClient;
+
+
+    protected AbstractAPIKeyNonceWSClient() {
+        super();
+    }
+
+    protected AbstractAPIKeyNonceWSClient(String authHeader, RestTemplate restTemplate) {
+        super(authHeader, restTemplate);
+    }
+
+    protected AbstractAPIKeyNonceWSClient(String authHeader, NonceClient nonceClient, RestTemplate restTemplate, CredentialClient credentialClient) {
+        super(authHeader, restTemplate);
+        this.credentialClient = credentialClient;
+        this.nonceClient = nonceClient;
+    }
+
+    public NonceClient getNonceClient() {
+        return nonceClient;
+    }
+
+    public void setNonceClient(NonceClient nonceClient) {
+        this.nonceClient = nonceClient;
+    }
+
+    public CredentialClient getCredentialClient() {
+        return credentialClient;
+    }
+
+    public void setCredentialClient(CredentialClient credentialClient) {
+        this.credentialClient = credentialClient;
+    }
+
+    private HttpEntity<String> generateHeader(final String personId, final MediaType mediaType) {
+        WsNonce nonce = null;
+        WsSessionCredential sessionCredential = (WsSessionCredential) credentialClient.getCredential(personId);
+        SessionNonceHmacCredential sessionNonceHmacCredential = new SessionNonceHmacCredential(sessionCredential);
+
+        nonce = nonceClient.getNonce(sessionNonceHmacCredential.getId());
+
+        String authHeaderValue = null;
+        try {
+            authHeaderValue = sessionNonceHmacCredential.generateHeader(nonce);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(authHeader, authHeaderValue);
+        headers.setAccept(Arrays.asList(mediaType));
+
+        return new HttpEntity<String>("parameters", headers);
+    }
+
+    protected <E> List<E> makeWSCall(final Class<E> type, final String personId, final String url, final MediaType mediaType) {
+        if (type != null && personId != null && url != null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("url : " + url);
+            }
+            HttpEntity<String> httpEntity = generateHeader(personId, mediaType);
+            return makeGenericWSCall(type, url, httpEntity);
+        } else
+            throw new IllegalArgumentException("type == null || personId == null || url == null");
+    }
+
+    protected <E> E makeWSCallSingleton(final Class<E> type, final String personId, final String url, final MediaType mediaType) {
+        if (type != null && personId != null && url != null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("url : " + url);
+            }
+
+            HttpEntity<String> httpEntity = generateHeader(personId, mediaType);
+            return makeGenericWSCallSingleton(type, url, httpEntity);
+        } else
+            throw new IllegalArgumentException("type == null || personId == null || url == null");
+    }
+
+
+}
--- commons-clients/src/main/java/edu/byu/core/common/wsAuth/wsSession/WsSessionClientDaoImpl.java
+++ commons-clients/src/main/java/edu/byu/core/common/wsAuth/wsSession/WsSessionClientDaoImpl.java
@@ -5,6 +5,7 @@ import edu.byu.core.common.wsAuth.api.Credential;
 import edu.byu.core.common.wsAuth.api.CredentialClient;
 import edu.byu.core.common.wsAuth.dao.WsSessionCredentialDAO;
 import edu.byu.core.common.wsAuth.model.hibernate.WsSessionCredential;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ import java.util.List;
  */
 @Service("wsSessionClientDao")
 public class WsSessionClientDaoImpl implements CredentialClient {
+    private final Logger LOG = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
     @Autowired(required = true)
     @Qualifier("wsSessionCredentialDAO")
@@ -27,9 +29,18 @@ public class WsSessionClientDaoImpl implements CredentialClient {
     public WsSessionCredential getCredential(final String personId) {
         if (personId != null) {
             List<WsSessionCredential> sessionCredentials = wsSessionCredentialDAO.getActiveSessions(personId);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(personId + " has the following number of sessionCredentials : " + sessionCredentials.size());
+            }
             if (sessionCredentials.isEmpty()) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Creating new session.");
+                }
                 return wsSessionCredentialDAO.createSession(personId);
             } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Using session : " + sessionCredentials.get(0));
+                }
                 return sessionCredentials.get(0);
             }
         } else
