e96b988da375ad5c41d6f088218cef4a97c37035
tefreestone <tefreestone@gmail.com>
2014-06-23 16:32:10 -0600

    refactored jaxb bean

--- commons-clients/src/main/java/edu/byu/core/common/wsAuth/model/xml/Nonce.java
+++ commons-clients/src/main/java/edu/byu/core/common/wsAuth/model/xml/Nonce.java
@@ -2,41 +2,44 @@ package edu.byu.core.common.wsAuth.model.xml;
 
 //import edu.byu.ws.authentication.domain.WsNonce;
 
+import edu.byu.core.common.wsAuth.model.hibernate.WsNonce;
+
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement(name = "nonce")
 @XmlType(namespace = "http://ws.byu.edu/namespaces/security/authentication/v1.0")
 public final class Nonce {
-//
-//    private String nonce;
-//    private Long nonceKey;
-//
-//    public Nonce() {
-//    }
-//
-//    @XmlElement(name = "nonceValue", required = true)
-//    public String getNonce() {
-//        return nonce;
-//    }
-//
-//    public void setNonce(String nonce) {
-//        this.nonce = nonce;
-//    }
-//
-//    @XmlElement(name = "nonceKey", required = true)
-//    public Long getNonceKey() {
-//        return nonceKey;
-//    }
-//
-//    public void setNonceKey(Long nonceKey) {
-//        this.nonceKey = nonceKey;
-//    }
-//
-//    public WsNonce convertToWsNonce() {
-//        WsNonce wsNonce = new WsNonce();
-//        wsNonce.setNonceKey(this.nonceKey);
-//        wsNonce.setNonceValue(this.nonce);
-//        return wsNonce;
-//    }
+
+    private String nonce;
+    private Long nonceKey;
+
+    public Nonce() {
+    }
+
+    @XmlElement(name = "nonceValue", required = true)
+    public String getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
+
+    @XmlElement(name = "nonceKey", required = true)
+    public Long getNonceKey() {
+        return nonceKey;
+    }
+
+    public void setNonceKey(Long nonceKey) {
+        this.nonceKey = nonceKey;
+    }
+
+    public WsNonce convertToWsNonce() {
+        WsNonce wsNonce = new WsNonce();
+        wsNonce.setNonceKey(this.nonceKey);
+        wsNonce.setNonceValue(this.nonce);
+        return wsNonce;
+    }
 }
